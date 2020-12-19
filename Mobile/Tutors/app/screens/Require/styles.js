b3db0da48ef4f4d6e8e13b647c9727e95723c821
pariks3 <75883595+pariks3@users.noreply.github.com>
2020-12-19 14:32:37 +0700

    insertImage

--- Mobile/Tutors/app/screens/Require/styles.js
+++ Mobile/Tutors/app/screens/Require/styles.js
@@ -1,10 +1,11 @@
 import { Platform, StyleSheet, StatusBar } from "react-native";
+
 import { colors } from "../../config/colors";
 
 export const styles = StyleSheet.create({
   contrainer: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: colors.white,
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   inputItem: {
@@ -12,25 +13,32 @@ export const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     backgroundColor: "#fff",
-    flex:1,
+    flex: 1,
   },
-  loginBtnWrapper: {
-    marginTop: 10,
-    marginLeft: 20,
-    marginRight: 20,
+  inputItem2: {
+    margin: 5,
+    alignItems: "flex-end",
+    backgroundColor: "#fff",
+    flex: 1,
+  },
+  itemWrapper: {
+    margin: 5,
+    flexDirection: "row",
+    flex: 0.6,
+    flexWrap: "wrap",
   },
   centeredView: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22
+    marginTop: 22,
   },
   modalView: {
-    margin: 20,
-    backgroundColor: "#fff",
-    borderRadius: 20,
+    margin: 0,
+    backgroundColor: "#acc",
+    borderRadius: 30,
     padding: 20,
-    alignItems: "center",
+    alignItems: "stretch",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -38,21 +46,15 @@ export const styles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5
+    elevation: 2,
+
   },
   openButton: {
     backgroundColor: "#BAE367",
-    borderRadius: 20,
-    paddingHorizontal: 70,
-    paddingVertical: 20,
+    borderRadius: 30,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
     elevation: 2,
-  },
-  timeButton: {
-    backgroundColor: "#BAE367",
-    borderRadius: 20,
-    paddingHorizontal: 70,
-    paddingVertical: 5,
-    flex:0.5,
   },
   closeButton: {
     backgroundColor: "#BAE367",
@@ -61,7 +63,7 @@ export const styles = StyleSheet.create({
     paddingVertical: 5,
     elevation: 2,
     alignSelf: "flex-end",
-    
+
   },
   textStyle: {
     color: "white",
@@ -72,19 +74,59 @@ export const styles = StyleSheet.create({
     textAlign: "center"
   },
   groupCheckBox: {
-    alignContent:"stretch",
-    alignItems: "stretch",
-    backgroundColor: "#fff",
-    paddingBottom: 20, 
+    justifyContent:"flex-start",
+    backgroundColor: "#fdf",
+    paddingBottom: 20,
   },
   alignCheckBox: {
     flexDirection: "row",
     alignItems: "center",
     backgroundColor: "#fff",
   },
-  textTime:{
-    fontSize: 16,
+  Global: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    backgroundColor: "#fff",
+  },
+  textInput: {
+    backgroundColor: colors.bg,
+    borderRadius: 15,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    flex: 0.8,
+  },
+  textDate: {
+    backgroundColor: colors.bg,
+    borderRadius: 15,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    flex: 0.8,
+    paddingVertical: 13,
+    // justifyContent: "space-between",
+  },
+  drop: {
+    height: 20, 
+    width: 200, 
+    justifyContent: "space-between", 
+    fontSize: 20
+  },
+  imageStyle: {
+    width: 20,
+    height: 20,
+  
+  },
+  mySelf: {
+    alignSelf: "flex-end",
 
+  },
+  wrapText:{
+    flexDirection: "row",
+    flexWrap: "wrap",
+ 
   }
 
 });
