7d6975f6615e1086e140954c1b7de51262d942e9
hongbao <pao22304.ar@gmail.com>
2020-12-21 21:19:55 +0700

    Signup Screen Done

--- Mobile/Tutors/app/screens/SignUp/SignUp.js
+++ Mobile/Tutors/app/screens/SignUp/SignUp.js
@@ -1,42 +1,65 @@
-import React from "react";
-import { SafeAreaView, Text, View } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import CheckBox from "@react-native-community/checkbox";
+import React, { useState } from "react";
+import { Text, View, ScrollView } from "react-native";
 import { PrimaryButton } from "../../components/buttons/PrimaryButton";
 import { SecondaryButton } from "../../components/buttons/SecondaryButton/SecondaryButton";
 import { PrimaryInput } from "../../components/forms/PrimaryInput/PrimaryInput";
 import { styles } from "./styles";
+import { colors } from "../../config/colors";
 
 const SignUp = ({ navigation }) => {
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.textTitle}>
-        <Text>Sign Up</Text>
-      </View>
-      <View style={styles.btnWrap}>
-        <SecondaryButton
-          style={styles.btnItemWrap}
-          label={"Continue With Facebook"}
-          btnType={"FACEBOOK"}
-          background={"dodgerblue"}
-          fontColor={"white"}
-        />
-      </View>
-      <View style={styles.textOr}>
-        <Text>Or Sign up with E-mail</Text>
-      </View>
-      <View style={styles.inputWrap}>
-        <View style={styles.inputItem}>
-          <PrimaryInput placeHolder={"Email Address"} />
+    // eslint-disable-next-line react-native/no-inline-styles
+    <ScrollView style={{ margin: 0, backgroundColor: "white" }}>
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
+          <Text style={styles.textTitle}>Sign Up</Text>
         </View>
-        <View style={styles.inputItem}>
-          <PrimaryInput placeHolder={"Username"} />
+        <View style={styles.btnWrapper}>
+          <SecondaryButton
+            label={"Sign Up With Facebook"}
+            btnType={"FACEBOOK"}
+            background={"dodgerblue"}
+            fontColor={"white"}
+          />
         </View>
-        <View style={styles.inputItem}>
-          <PrimaryInput placeHolder={"Password"} />
+        <Text style={styles.textOr}>Or Sign up with E-mail</Text>
+
+        <View style={styles.inputWrap}>
+          <View style={styles.inputItem}>
+            <PrimaryInput placeHolder={"Email Address"} />
+          </View>
+          <View style={styles.inputItem}>
+            <PrimaryInput placeHolder={"Username"} />
+          </View>
+          <View style={styles.inputItem}>
+            <PrimaryInput placeHolder={"Password"} />
+          </View>
+          <View style={styles.policy}>
+            <Text style={styles.policyText}>
+              I have read the{" "}
+              <Text
+                style={styles.policyLink}
+                onPress={() => navigation.navigate("SignUp")}
+              >
+                Private Policy
+              </Text>
+            </Text>
+            <View style={styles.policyCheckBox}>
+              <CheckBox
+                disabled={false}
+                value={toggleCheckBox}
+                onCheckColor={"green"}
+                tintColor={"red"}
+                onValueChange={(newValue) => setToggleCheckBox(newValue)}
+              />
+            </View>
+          </View>
         </View>
-      </View>
-      <View style={styles.SignUpBtnWrapper}>
-        <PrimaryButton label={"Sign Up"} screenName={"Login"} />
+        <View style={styles.btnWrapper}>
+          <PrimaryButton label={"Sign Up"} screenName={"Login"} />
+        </View>
       </View>
     </ScrollView>
   );
--- Mobile/Tutors/app/screens/SignUp/styles.js
+++ Mobile/Tutors/app/screens/SignUp/styles.js
@@ -1,34 +1,68 @@
-import { Platform, StatusBar, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { colors } from "../../config/colors";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: colors.white,
+    justifyContent: "center",
+    padding: 5,
     // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
-  textTitle: {
-    backgroundColor: "white",
+  textContainer: {
+    backgroundColor: colors.white,
     flex: 1,
+    alignItems: "center",
+    marginTop: 30,
   },
-  btnItemWrap: {
-    flex: 1,
+  textTitle: {
+    fontSize: 30,
+    fontStyle: "normal",
+    color: colors.primary,
   },
   textOr: {
     flex: 1,
-    alignSelf: "center",
     textAlign: "center",
+    margin: 30,
+    // fontFamily: "HelveticaNeue",
+    fontSize: 14,
+    fontWeight: "700",
+    lineHeight: 30,
+    color: colors.gray,
   },
   inputWrap: {
     flex: 1,
+    margin: 10,
   },
   inputItem: {
     flex: 1,
+    marginBottom: 10,
     flexDirection: "column",
     justifyContent: "space-around",
   },
-  SignUpBtnWrapper: {
+  policy: {
     flex: 1,
+    flexDirection: "row",
+  },
+  policyText: {
+    flex: 1,
+    marginLeft: 18,
+    marginTop: 5,
+  },
+  policyCheckBox: {
+    flex: 0.5,
+    flexDirection: "row",
     justifyContent: "flex-end",
+  },
+  policyLink: {
+    color: colors.primary,
+  },
+  btnWrapper: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: colors.white,
+    marginTop: 40,
+    marginLeft: 10,
+    marginRight: 10,
   },
 });
