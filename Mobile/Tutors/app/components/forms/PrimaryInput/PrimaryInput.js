8fc3d22e60662920176d2ca2c1f63551aade4d37
hongbao <pao22304.ar@gmail.com>
2021-01-06 21:53:24 +0700

    Co-authored-by: Manusawe Wongboonpheng <B6071110@office365.sut.ac.th>

--- Mobile/Tutors/app/components/forms/PrimaryInput/PrimaryInput.js
+++ Mobile/Tutors/app/components/forms/PrimaryInput/PrimaryInput.js
@@ -3,10 +3,16 @@ import { View, StyleSheet, TextInput, Image } from "react-native";
 import { colors } from "../../../config/colors";
 
 export const PrimaryInput = (props) => {
-  const { placeHolder, isValid, value } = props;
+  const { placeHolder, isValid, value, onChangeText } = props;
+
   return (
     <View style={styles.container}>
-      <TextInput value={value} style={styles.input} placeholder={placeHolder} />
+      <TextInput
+        value={value}
+        style={styles.input}
+        placeholder={placeHolder}
+        onChangeText={onChangeText}
+      />
       <View style={styles.validityShowWrapper}>
         {isValid ? (
           <Image source={require("../../../assets/images/checked.png")} />
