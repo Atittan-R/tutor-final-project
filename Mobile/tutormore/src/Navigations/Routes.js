45a8768daca121459f8356eaf43c0efee09c6b61
hongbao <pao22304.ar@gmail.com>
2021-01-11 03:07:33 +0700

    Separated View between User and Tutor

--- Mobile/tutormore/src/Navigations/Routes.js
+++ Mobile/tutormore/src/Navigations/Routes.js
@@ -1,59 +1,62 @@
-import React from "react";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import React, {useEffect, useState} from "react";
+import {NavigationContainer, useNavigation} from "@react-navigation/native";
 import AuthenticationStack from "./AuthenticationStack";
-import { PrivilegeUser, PrivilegeTutor } from "./Privilege";
-import { useGlobalVar } from "../context/GlobalContex";
-import { Text, View } from "react-native";
+import {PrivilegeUser, PrivilegeTutor} from "./Privilege";
+import {useGlobalVar} from "../context/GlobalContex";
+import {Platform, StatusBar, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+// import RoleSelection, {styles} from "./RoleSelection";
+import Colors from "../configs/Colors";
+import CharacterMrTeacherFullBody from "../assets/characters/CharacterMrTeacherFullBody";
+import CharacterStudentFullBody from "../assets/characters/CharacterStudentFullBody";
+import RoleSelection from "../screens/Authentication/RoleSelection";
 
 function SplashScreen() {
-  return (
-    <View>
-      <Text>Loading...</Text>
-      <Text>Loading...</Text>
-      <Text>Loading...</Text>
-      <Text>Loading...</Text>
-      <Text>Loading...</Text>
-      <Text>Loading...</Text>
-      <Text>Loading...</Text>
-      <Text>Loading..</Text>
-    </View>
-  );
+    return (
+        <View>
+            <Text>Loading...</Text>
+            <Text>Loading...</Text>
+            <Text>Loading...</Text>
+            <Text>Loading...</Text>
+            <Text>Loading...</Text>
+            <Text>Loading...</Text>
+            <Text>Loading...</Text>
+            <Text>Loading..</Text>
+        </View>
+    );
 }
 
-function AskingRole() {
-  return (
-    <View>
-      <Text>AskingRole...</Text>
-      <Text>AskingRole...</Text>
-      <Text>AskingRole...</Text>
-      <Text>AskingRole...</Text>
-      <Text>AskingRole...</Text>
-      <Text>AskingRole...</Text>
-      <Text>AskingRole...</Text>
-    </View>
-  );
-}
-const renderingCheck = () => {
-  const { authentication, current_user } = useGlobalVar();
-  const [currentuser, setUserInfo] = current_user;
-  const [state, dispatch] = authentication;
-  
-  const role_router = {
-    ROLE_TUTOR: <PrivilegeTutor />,
-    ROLE_USER: <PrivilegeUser />,
-  };
-
-  state.isLoading && SplashScreen();
-
-  return state.userToken === null ? (
-    <AuthenticationStack />
-  ) : currentuser.roles.length !== 1 ? (
-    AskingRole()
-  ) : (
-    role_router[currentuser.roles]
-  );
+export const role_router = {
+    ROLE_TUTOR: <PrivilegeTutor/>,
+    ROLE_USER: <PrivilegeUser/>,
 };
 
+export const renderingCheck = () => {
+    const {authentication, current_user, roleglobal} = useGlobalVar();
+    const [roleselection] = roleglobal
+    const [currentuser] = current_user;
+    const [state] = authentication;
+
+    //IF isLoading is true will be show Load Screen
+    state.isLoading && SplashScreen();
+
+    //Prepare variable for role that will be select in RoleSelection.js
+    let {role} = "";
+    roleselection !== undefined ? {role} = roleselection : "";
+
+    // Use on product
+    // return state.userToken === null
+    //     ? <AuthenticationStack />
+    //     : currentuser.roles.length !== 1
+    //         ? (state.userRole === null || state.userRole === undefined)
+    //         ? <RoleSelection/>
+    //         : role_router[role]
+    //     : role_router[currentuser.roles]
+
+    //Test UI with out login
+    return (state.userRole === null || state.userRole === undefined)
+        ? <RoleSelection/>
+        : role_router[role]
+};
 export default function Routes() {
-  return <NavigationContainer>{renderingCheck()}</NavigationContainer>;
+    return <NavigationContainer>{renderingCheck()}</NavigationContainer>;
 }
--- Mobile/tutormore/src/context/GlobalContex.js
+++ Mobile/tutormore/src/context/GlobalContex.js
@@ -1,84 +1,99 @@
-import React, { useState, createContext, useContext, useMemo } from "react";
-import { useReducer } from "react";
+import React, {useState, createContext, useContext, useMemo} from "react";
+import {useReducer} from "react";
 import axios from "axios";
 
 const GlobalVarContext = createContext();
 
-export const GlobalProvider = ({ children }) => {
-  const [current_user, setUserInfo] = useState();
+export const GlobalProvider = ({children}) => {
+    const [current_user, setUserInfo] = useState();
+    const [roleselection, setRoleSection] = useState();
 
-  const [state, dispatch] = useReducer(
-    (prevState, action) => {
-      //reducer
-      switch (action.type) {
-        case "RESTORE_TOKEN":
-          return {
-            ...prevState,
-            userToken: action.token,
-            isLoading: false,
-          };
-        case "SIGN_IN":
-          return {
-            ...prevState,
-            isSignout: false,
-            userToken: action.token,
-          };
-        case "SIGN_OUT":
-          return {
-            ...prevState,
-            isSignout: true,
-            userToken: null,
-          };
-      }
-    },
-    //initial State
-    {
-      isLoading: true,
-      isSignout: false,
-      userToken: null,
-    }
-  );
-
-  const auth = useMemo(
-    () => ({
-      signIn: async (data) => {
-        try {
-          const user_token = await axios.post(
-            "http://192.168.1.62:3986/api/auth/signin",
-            {
-              email: data.email,
-              password: data.password,
+    const [state, dispatch] = useReducer(
+        (prevState, action) => {
+            //reducer
+            switch (action.type) {
+                case "RESTORE_TOKEN":
+                    return {
+                        ...prevState,
+                        userToken: action.token,
+                        isLoading: false,
+                    };
+                case "SIGN_IN":
+                    return {
+                        ...prevState,
+                        isSignout: false,
+                        userToken: action.token,
+                    };
+                case "SIGN_OUT":
+                    return {
+                        ...prevState,
+                        isSignout: true,
+                        userToken: null,
+                        userRole: null,
+                    };
+                case "ROLE_ENTRY":
+                    return {
+                        ...prevState,
+                        isSignout: false,
+                        userToken: action.token,
+                        userRole: action.role,
+                    };
             }
-          );
-          console.log("data signin from Global state", data);
-          setUserInfo(user_token.data);
-          dispatch({ type: "SIGN_IN", token: user_token.data.accessToken });
-        } catch (error) {
-          //TODO Cath error to show on UI
-          console.error(error);
+        },
+        //initial State
+        {
+            isLoading: true,
+            isSignout: false,
+            userToken: null,
+            userRole: null,
         }
-      },
-      signOut: () => dispatch({ type: "SIGN_OUT" }),
-      signUp: async (data) => {
-        //TODO Sign Up API
-        console.log("data signup from Global state", data);
-        dispatch({ type: "SIGN_IN", token: "dummy-auth-token" });
-      },
-    }),
-    []
-  );
+    );
 
-  return (
-    <GlobalVarContext.Provider
-      value={{
-        authentication: [state, dispatch],
-        auth: auth,
-        current_user: [current_user, setUserInfo],
-      }}
-    >
-      {children}
-    </GlobalVarContext.Provider>
-  );
+    const auth = useMemo(
+        () => ({
+            signIn: async (data) => {
+                try {
+                    const user_token = await axios.post(
+                        "http://192.168.1.62:3986/api/auth/signin",
+                        {
+                            email: data.email,
+                            password: data.password,
+                        }
+                    );
+                    console.log("data signin from Global state", data);
+                    setUserInfo(user_token.data);
+                    dispatch({type: "SIGN_IN", token: user_token.data.accessToken});
+                } catch (error) {
+                    //TODO Cath error to show on UI
+                    console.error(error);
+                }
+            },
+            signOut: () => dispatch({type: "SIGN_OUT"}),
+            signUp: async (data) => {
+                //TODO Sign Up API
+                console.log("data signup from Global state", data);
+                dispatch({type: "SIGN_IN", token: "dummy-auth-token"});
+            },
+            roleEntry: async (data) => {
+                setRoleSection(data);
+                dispatch({type: "ROLE_ENTRY", role: data})
+            },
+        }),
+        []
+    );
+
+    return (
+        <GlobalVarContext.Provider
+            value={{
+                authentication: [state, dispatch],
+                auth: auth,
+                current_user: [current_user, setUserInfo],
+                roleglobal: [roleselection, setRoleSection],
+            }}
+        >
+            {children}
+        </GlobalVarContext.Provider>
+    );
 };
 
 export const useGlobalVar = () => useContext(GlobalVarContext);
