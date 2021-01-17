3740e24afe591046af95c27fc240a0f3c5ca17e4
hongbao <pao22304.ar@gmail.com>
2021-01-17 12:05:42 +0700

    space

--- Mobile/tutormore/src/screens/Authentication/Major.js
+++ Mobile/tutormore/src/screens/Authentication/Major.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {Button, Text, View} from "react-native";
 
 export default function Major({navigation}) {
-  return (
-    <View>
-      <Text>Major</Text>
-      <Button
-        title={"Tutor Register"}
-        onPress={() => navigation.push("TutorRegister")}
-      ></Button>
-    </View>
-  );
+    return (
+        <View>
+            <Text>Major</Text>
+            <Button
+                title={"Tutor Register"}
+                onPress={() => navigation.push("TutorRegister")}
+            />
+        </View>
+    );
 }
-
-const styles = StyleSheet.create({});
