428e4e6322331d0ee7a70254830c184a066731d6
hongbao <pao22304.ar@gmail.com>
2021-01-17 17:28:57 +0700

    Commit From Route

--- Mobile/tutormore/src/screens/UserScreen/Feed.js
+++ Mobile/tutormore/src/screens/UserScreen/Feed.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 import {
   FlatList,
   Image,
@@ -7,236 +7,163 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  View,
-} from "react-native";
-import { Icon } from "react-native-elements";
-import Colors from "../../configs/Colors";
-import API from "../../services/API";
+  View
+} from 'react-native'
+import { Icon } from 'react-native-elements';
+import Colors from '../../configs/Colors';
+import API from "../../services/API"
 
 export default function Feed({ navigation }) {
-  const [count, setCount] = useState(0);
-  const [dataList, setDataList] = useState([]);
-  const data = [
-    {
-      id: 1,
-      name: "pixels dragon x",
-      course: "Database",
-      date: "Mon Wed Fri",
-      time: "17.0-21.0",
-    },
-    {
-      id: 2,
-      name: "ruin force",
-      course: "Com pro1",
-      date: "Sun Mon Tue Wed Fri Sat",
-      time: "17.0-21.0",
-    },
-    {
-      id: 3,
-      name: "michael rayder",
-      course: "Data Com",
-      date: "Everyday",
-      time: "17.0-21.0",
-    },
-    {
-      id: 4,
-      name: "lucius flux",
-      course: "HCI",
-      date: "Mon Wed Fri",
-      time: "17.0-21.0",
-    },
-    {
-      id: 5,
-      name: "kuro monitor",
-      course: "Math for Com",
-      date: "Mon Wed Fri",
-      time: "17.0-21.0",
-    },
-    {
-      id: 6,
-      name: "pixels dragon x",
-      course: "Database",
-      date: "Mon Wed Fri",
-      time: "17.0-21.0",
-    },
-    {
-      id: 7,
-      name: "ruin force",
-      course: "Com pro1",
-      date: "Sun Mon Tue Wed Fri Sat",
-      time: "17.0-21.0",
-    },
-    {
-      id: 8,
-      name: "michael rayder",
-      course: "Data Com",
-      date: "Everyday",
-      time: "17.0-21.0",
-    },
-    {
-      id: 9,
-      name: "lucius flux",
-      course: "HCI",
-      date: "Mon Wed Fri",
-      time: "17.0-21.0",
-    },
-    {
-      id: 10,
-      name: "kuro monitor",
-      course: "Math for Com",
-      date: "Mon Wed Fri",
-      time: "17.0-21.0",
-    },
-    {
-      id: 11,
-      name: "pixels dragon x",
-      course: "Database",
-      date: "Mon Wed Fri",
-      time: "17.0-21.0",
-    },
-    {
-      id: 12,
-      name: "ruin force",
-      course: "Com pro1",
-      date: "Sun Mon Tue Wed Fri Sat",
-      time: "17.0-21.0",
-    },
-    {
-      id: 13,
-      name: "michael rayder",
-      course: "Data Com",
-      date: "Everyday",
-      time: "17.0-21.0",
-    },
-    {
-      id: 14,
-      name: "lucius flux",
-      course: "HCI",
-      date: "Mon Wed Fri",
-      time: "17.0-21.0",
-    },
-    {
-      id: 15,
-      name: "kuro monitor",
-      course: "Math for Com",
-      date: "Mon Wed Fri",
-      time: "17.0-21.0",
-    },
-  ];
+  const [request, setRequest] = useState([]);
+  const [isjoin, setisJoin] = useState([]);
 
-  useEffect(() => {
-    const findRequest = async () => {
+  // ปรับApi request/findAll ให้กับ ui
+  // const fetchApi= async()=>{
+
+  //         const fetch_req = await API.get("/request/findAll");
+  //         // console.log("Log: ", fetch_req.data);
+  //         setRequest(fetch_req.data)
+  //         console.log("Log: ", request);
+
+  const join = async (resId, userId) => {
+    try {
+      const join_req = await API.post("join", {
+        userId: userId, requestId: resId
+      });
+      console.log(join_req.data.status);
+      isjoin.push({id:resId})
+      setisJoin([...isjoin,{id:resId}])
+      console.log(isjoin);
+    } catch (error) {
+      console.log('====================================');
+      console.log(error);
+      console.log('====================================');
+
+    }
+  }
+
+  const cancel = async (resId, userId) => {
+    try {
+      const cancel_join = await API.post("join/cancel", {
+        userId: userId, requestId: resId
+      });
+      console.log(cancel_join.data);
+      // isjoin.push({id:resId})
+      setisJoin(isjoin.filter(x => x.id !== resId))
+      console.log(isjoin);
+    } catch (error) {
+      console.log('====================================');
+      console.log(error);
+      console.log('====================================');
+    }
+  }
+  useEffect(async () => {
+    const fetchApi = async () => {
       try {
-        const data = await API.get("/request/findAll");
-        setDataList(data.data.request);
+        const fetch_req = await API.get("/request/findAll");
+        const fetch_join = await API.post("/user/join", {
+          userId: 2,
+
+        });
+        console.log((fetch_join));
+        setRequest(fetch_req.data.request)
+        setisJoin(fetch_join.data)
+
       } catch (error) {
-        console.log(error.response.data.message);
+        console.log(error);
       }
-    };
-    findRequest();
-  }, []);
+    }
+    fetchApi();
+  }, [])
 
-  console.log(
-    "data from outside:",
-    dataList.map((i) => i.name)
-  );
-
+  const [count, setCount] = useState(0);
   return (
-    <>
-      {/* // header */}
-      <SafeAreaView style={styles.container} />
-      <View style={styles.headerBar}>
-        <TouchableOpacity
-          style={{ color: Colors.secondary, marginRight: 10 }}
-          onPress={() => navigation.navigate("Home")}
-        >
-          <Icon
-            name="arrow-back-outline"
-            type="ionicon"
-            color={Colors.secondary}
-          />
-        </TouchableOpacity>
-        <Text style={styles.textHeader}>Feed Request</Text>
-        <TouchableOpacity
-          style={styles.search}
-          onPress={() => navigation.push("SearchRequest")}
-        >
-          <Text style={{ color: Colors.secondary }}>search</Text>
-          <Icon
-            name="search-outline"
-            type="ionicon"
-            style={{ color: Colors.secondary }}
-          />
-        </TouchableOpacity>
-      </View>
-
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View style={styles.cardView}>
-            <View style={styles.viewItem}>
-              <Image
-                source={require("../../assets/profile.jpg")}
-                style={styles.image}
-              />
-              <Text style={styles.title}>{item.name}</Text>
-            </View>
-            <View
-              style={{
-                marginTop: 5,
-                borderTopColor: Colors.gray,
-                borderTopWidth: 1,
-                display: "flex",
-                flexWrap: "wrap",
-                flexDirection: "row",
-                justifyContent: "space-between",
-              }}
+      <>
+        {/* header */}
+        <SafeAreaView style={styles.container}>
+          <View style={styles.headerBar}>
+            <TouchableOpacity
+                style={{ color: Colors.secondary, marginRight: 10 }}
+                onPress={() => navigation.navigate("Home")}>
+              <Icon name="arrow-back-outline" type="ionicon" color={Colors.secondary} />
+            </TouchableOpacity>
+            <Text style={styles.textHeader}>Feed Request</Text>
+            <TouchableOpacity
+                style={styles.search}
+                onPress={() => navigation.push("SearchRequest")}
             >
-              <View>
-                <View style={styles.viewItem}>
-                  <Icon
-                    name="book"
-                    type="material"
-                    color={Colors.secondary}
-                    style={styles.icon}
-                  />
-                  <Text style={styles.title}>{item.title}</Text>
-                </View>
-                <View style={styles.viewItem}>
-                  <Icon
-                    name="event"
-                    type="material"
-                    color={Colors.secondary}
-                    style={styles.icon}
-                  />
-                  <Text style={styles.text}>{item.date}</Text>
-                </View>
-                <View style={styles.viewItem}>
-                  <Icon
-                    name="schedule"
-                    type="material"
-                    color={Colors.secondary}
-                    style={styles.icon}
-                  />
-                  <Text style={styles.text}>{item.time}</Text>
-                </View>
-              </View>
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => setCount((cnt) => cnt + 1)}
-              >
-                <Text style={styles.text}>Join</Text>
-                <Text style={{ fontSize: 12, color: Colors.secondary }}>
-                  +{count}
-                </Text>
-              </TouchableOpacity>
-            </View>
+              <Text style={{ color: Colors.secondary }}>search</Text>
+              <Icon
+                  name="search-outline"
+                  type="ionicon"
+                  style={{ color: Colors.secondary }}
+              />
+            </TouchableOpacity>
           </View>
-        )}
-      />
-    </>
-  );
+
+          <FlatList
+              data={request}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) =>
+                  (
+                      <View style={styles.cardView}>
+                        <View style={styles.viewItem}>
+                          <Image source={require("../../assets/profile.jpg")} style={styles.image} />
+                          <Text style={styles.title}>{item.user.username}</Text>
+                        </View>
+                        <View
+                            style={{
+                              marginTop: 5,
+                              borderTopColor: Colors.gray,
+                              borderTopWidth: 1,
+                              display: "flex",
+                              flexWrap: "wrap",
+                              flexDirection: "row",
+                              justifyContent: "space-between",
+                            }}>
+                          <View>
+                            <View style={styles.viewItem}>
+                              <Icon name="book" type="material" color={Colors.secondary} style={styles.icon} />
+                              <Text style={styles.title}>{item.name}</Text>
+                            </View>
+                            <View style={styles.viewItem}>
+                              <Icon name="event" type="material" color={Colors.secondary}
+                                    style={styles.icon} />
+                              <Text style={styles.text}>{item.date}</Text>
+                            </View>
+                            <View style={styles.viewItem}>
+                              <Icon name="schedule" type="material" color={Colors.secondary}
+                                    style={styles.icon} />
+                              {/* <Text style={styles.text}>{item.time}</Text> */}
+                              <Text style={styles.text}>{item.time_start}-{item.time_end}</Text>
+                            </View>
+                          </View>
+                          {
+
+                            isjoin.map((i)=>i.id).includes(item.id) ?
+                                <TouchableOpacity style={styles.button_cancel}
+                                                  onPress={() =>
+                                                      cancel(item.id, 2)
+                                                  }>
+                                  <Text style={styles.text}>cancel</Text>
+                                </TouchableOpacity>
+                                : <TouchableOpacity style={styles.button} onPress={() =>
+                                    join(item.id, 2)
+                                  // setCount((cnt) => cnt + 1)
+                                }>
+                                  <Text style={styles.text}>Join</Text>
+                                  <Text style={{ fontSize: 12, color: Colors.secondary }}>+{count}</Text>
+                                </TouchableOpacity>
+
+
+                          }
+
+                        </View>
+                      </View>)} />
+          {/* <Text>{request}</Text> */}
+        </SafeAreaView>
+      </>
+  )
 }
 const styles = StyleSheet.create({
   container: {
@@ -251,7 +178,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingHorizontal: 20,
     paddingVertical: 10,
-    backgroundColor: Colors.primary,
+    backgroundColor: Colors.primary
   },
   textHeader: {
     fontSize: 20,
@@ -263,7 +190,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
     borderRadius: 5,
     padding: 10,
-    backgroundColor: Colors.white,
+    backgroundColor: Colors.white
   },
   viewItem: {
     flex: 1,
@@ -272,15 +199,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   text: {
-    color: Colors.secondary,
+    color: Colors.secondary
   },
   title: {
     fontWeight: "bold",
-    color: Colors.secondary,
+    color: Colors.secondary
   },
   icon: {
     marginRight: 10,
-    marginVertical: 2,
+    marginVertical: 2
   },
   image: {
     width: 30,
@@ -288,7 +215,7 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     borderWidth: 2,
     borderColor: Colors.primary,
-    marginRight: 10,
+    marginRight: 10
   },
   button: {
     backgroundColor: Colors.primary,
@@ -296,12 +223,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 5,
     borderRadius: 5,
     alignItems: "center",
-    justifyContent: "center",
-  },
-  add: {
-    padding: 10,
-    borderRadius: 30,
-    left: 120,
+    justifyContent: "center"
   },
   search: {
     flex: 1,
@@ -314,4 +236,17 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     backgroundColor: Colors.gray,
   },
-});
+  button_cancel: {
+    backgroundColor: Colors.gray,
+    paddingVertical: 5,
+    paddingHorizontal: 5,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  add: {
+    padding: 10,
+    borderRadius: 30,
+    left: 120
+  }
+})
